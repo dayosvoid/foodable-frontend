7b72596bdaf240b2e6a7bce5725f8463b17f827e
Adedayo <atimileyin911@gmail.com>
2026-05-17 15:45:37 +0100

    jjjjj

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  BASE_URL:import.meta.env. VITE_API_URL 
+  BASE_URL:import.meta.env.VITE_API_URL 
 })
 
 axiosInstance.interceptors.request.use((config)=>{
